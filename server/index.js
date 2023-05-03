2846e863d1a0e1c478b43095a359d23694d20407
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-03 15:57:56 -0600

    writing out a boilerplate for the backend

--- server/index.js
+++ server/index.js
@@ -1,2 +1,19 @@
-//!BOILERPLATE
-//
+//!BOILERPLATE FOR SERVER
+//*Import statements at the top, Global Variables/ Node
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const PORT = process.env.PORT || 8080
+const app = express() //can also be called server 
+
+//*Middleware (Defines HOW the server runs)/ Express
+app.use(express.json())
+app.use(cors())
+
+//*Endpoints (axios calls and HTTP requests)/ Express
+app.get('/api/info', async(req, res) => {
+    console.log('this is an express request')
+})
+
+//*Listen
+app.listen(PORT, () => console.log(`____Up on port ${PORT}____`))
